6762efef6ea004aa4a149b811c359ae3b05786bf
Ashish Kumar <ashishkummar@gmail.com>
2019-11-07 17:42:03 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"; 
+import ReactDOM from 'react-dom';
+import Video360Three from "./Video360Three";
+
+
+ReactDOM.render(<>
+ 
+ <Video360Three 
+autoRotate="true"
+width="769"
+height="600"
+/>
+
+</>, document.getElementById('root'));
+ 
